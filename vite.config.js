b987f688df72d56a963b9dd0c0f0cb35b2bca4fd
8VaNcRis <1v4ncr1s@gmail.com>
2025-03-08 14:41:16 +0800

    Fix homepage issue

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/Time-app/",  // ✅ Make sure this matches your repo name
+  base: "/Time-app/",
 });
 
